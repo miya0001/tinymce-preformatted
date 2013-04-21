87381ae879c4b5f744c124c355c57c4f429b2509
miya <miya@wpist.me>
2013-04-22 01:39:49 +0900

    ver 0.3.0

--- tags/0.1/mce_plugins/plugins/preformatted/langs/en_dlg.js
+++
@@ -1,4 +0,0 @@
-tinyMCE.addI18n('en.preformatted_dlg',{
-title:"Insert preformatted text",
-desc:"Insert preformatted text"
-});
--- tags/0.2/mce_plugins/plugins/preformatted/langs/ja_dlg.js
+++
@@ -1,4 +0,0 @@
-tinyMCE.addI18n('ja.preformatted_dlg',{
-title:"整形済みテキストを挿入",
-desc:"整形済みテキストを挿入"
-});
